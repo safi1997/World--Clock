ceaf5bb0c9af08850709fdbb092ccf23ce0ff489
Safolah / Safi <69314363+safi1997@users.noreply.github.com>
2023-01-11 20:59:45 +0100

    The asignment Wolrd Clock is Solved

--- src/tests/Select.test.js
+++ src/tests/Select.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
-import SelectZone from "../components/Select";
+import SelectZone from "../components/SelectZone";
 
 const countries = [
     { capital: "Zurich", zone: "Europe/Zurich", id: 1 },
-    { capital: "Berlin", zone: "Europe/Berlin", id:2 }
+    { capital: "Berlin", zone: "Europe/Berlin", id: 2 }
 ];
 
 test("renders select element with options for each country", () => {
@@ -28,8 +28,8 @@ test("calls setZone with the selected option value when the select element is ch
     render(<SelectZone setZone={setZone} countries={countries} />);
     const selectElement = screen.getByTestId("select-element");
 
-// Simulate a change event on a select element, setting the value to "Europe/Berlin"
-fireEvent.change(selectElement, { target: { value: "Europe/Berlin" } });
-// Assert that the setZone function was called with the argument "Europe/Berlin"
-expect(setZone).toHaveBeenCalledWith("Europe/Berlin");
+    // Simulate a change event on a select element, setting the value to "Europe/Berlin"
+    fireEvent.change(selectElement, { target: { value: "Europe/Berlin" } });
+    // Assert that the setZone function was called with the argument "Europe/Berlin"
+    expect(setZone).toHaveBeenCalledWith("Europe/Berlin");
 });
