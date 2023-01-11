6885a2dc84a9b0e8f347aa7b3df19c392736e906
Safolah / Safi <69314363+safi1997@users.noreply.github.com>
2023-01-11 14:18:44 +0100

    This is the Main Branch and i weana push the Assignment in main branch

---
+++ src/components/SelectZone.js
@@ -0,0 +1,10 @@
+import React from "react";
+const SelectZone = ({ setZone, countries }) => {
+  return (
+    <div className="content-container">
+      {/* select goes here  */}
+    </div>
+  );
+};
+
+export default SelectZone;
